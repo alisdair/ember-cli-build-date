38d3cfe18e5e2cf486c4e3293f74de510924fd3f
Chester Rivas <chester.rivas@cibc.com>
2016-10-03 12:27:43 -0400

    added moment.js to date

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-/* jshint node: true */
+
 'use strict';
+
+var moment = require('moment');
 
 module.exports = {
   name: 'ember-cli-build-date',
 
   config: function(env, baseConfig) {
     if (baseConfig.APP) {
-      baseConfig.APP.buildDate = new Date();
+      baseConfig.APP.buildDate = moment().format('MM/DD/YYYY h:mm:ss a');
     }
 
     return this._super.config.apply(this, arguments);
